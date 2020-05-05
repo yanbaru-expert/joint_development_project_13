74459b5e00a65aabc5c48c7eac5f2153691ab753
Naozumi Numata <naozumi225@gmail.com>
2020-05-05 14:41:36 +0900

    新規投稿機能(users)を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    #新規作成画面に対するアクション
+    def new
+        @user = User.new
+    end
+
+    #新規登録するためのアクション
+    def create
+        User.create(user_params)
+    end
+
+    #ストロングパラメーター
+    private
+    def user_params
+        params.require(:user).permit(:name, :age)
+    end
+
 end
