e803466881022c2e75a86db620a8b46b04b3c018
柴谷遼太朗 <shibataniryotaro@shibataniryoutarounoMacBook-Air.local>
2020-05-06 19:33:16 +0900

    created edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,15 @@ class UsersController < ApplicationController
 		User.create(user_params)
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		user = User.find(params[:id])
+		user.update(user_params)
+	end
+
 	def show
 		@user = User.find(params[:id])
 	end
