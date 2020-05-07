3eab0326033034f5f189756fb5358fa31f33d8a2
柴谷遼太朗 <shibataniryotaro@shibataniryoutarounoMacBook-Air.local>
2020-05-07 22:36:55 +0900

    created index function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,15 @@
 class MessagesController < ApplicationController
+	
+	def index
+    	@messages = Message.all
+  	end
+
 	def new
 		@message = Message.new
 	end
 
 	def create
-    	message.create(message_params)
+    	Message.create(message_params)
   	end
 
   	private
