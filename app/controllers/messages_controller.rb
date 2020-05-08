7c909891ded6e378d9d6b35f67774ba0f6786d7d
柴谷遼太朗 <shibataniryotaro@shibataniryoutarounoMacBook-Air.local>
2020-05-08 21:51:16 +0900

    created destroy function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,33 +1,38 @@
 class MessagesController < ApplicationController
 	
-	def index
-    	@messages = Message.all
-  	end
+  def index
+    @messages = Message.all
+  end
 
-	def new
-		@message = Message.new
-	end
+  def new
+    @message = Message.new
+  end
 
-	def create
-    	Message.create(message_params)
-  	end
+  def create
+    Message.create(message_params)
+  end
 
-  	def edit
-    	@message = Message.find(params[:id])
-  	end
+  def show
+	@message = Message.find(params[:id])
+  end
 
-  	def update
-    	message = Message.find(params[:id])
-    	message.update(message_params)
-  	end
+  def edit
+    @message = Message.find(params[:id])
+  end
 
-  	def show
-		@message = Message.find(params[:id])
-	end
+  # 以下のアクションを追加
+  def update
+    message = Message.find(params[:id])
+    message.update(message_params)
+  end
 
-  	private
+  def destroy
+    message = Message.find(params[:id])
+    message.destroy
+  end
 
-  	def message_params
+  private
+  def message_params
     params.require(:message).permit(:title, :content)
-  	end
+  end
 end
