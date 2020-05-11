e0495f15eb356edb1c240e13a14e30f0c8ae5567
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-05-11 17:33:48 +0900

    taskモデルとカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_130447) do
+ActiveRecord::Schema.define(version: 2020_05_11_082600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
